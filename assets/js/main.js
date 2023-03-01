b20b44eeb550ea7751ce6f0a45974dcdbba109c2
lilian-rangel <lilianrm.nutri@gmail.com>
2023-03-01 08:53:55 -0300

    :lipstick: feat: select the consts and add event listener

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const addBtn = document.getElementById('add')
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
+addBtn.addEventListener('click', () => addNewNote())
